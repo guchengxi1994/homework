bef3bb6bfe3e027305c2a0faaccefdf7ace57c08
guchengxi1994 <guchengxi1994@users.noreply.github.com>
2020-02-06 18:25:57 +0800

    0

--- house/test.py
+++ house/test.py
@@ -2,7 +2,12 @@ import cv2
 import numpy as np
 import copy
 
-img = cv2.imread("D:\\house\\extract_test.jpg")
+
+def powerFunc(x):
+    return 2**x - 1
+
+# img = cv2.imread("D:\\homework\\homework\\house\\lenna.png")
+img = cv2.imread("D:\\homework\\homework\\house\\extract_test.jpg")
 grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 minV = np.min(grayImg)
@@ -16,7 +21,7 @@ grayImg = np.array(grayImg,dtype=float)
 
 grayImg = np.log2((grayImg-minV)/maxV + 1)
 
-print(grayImg)
+# print(grayImg)
 
 c1 = copy.deepcopy(grayImg)
 c1[c1<=0.5] = 1                        #大于0.5
@@ -39,17 +44,33 @@ newMatrix = n1 + n2
 
 # newMatrix = 2*grayImg**2 if grayImg.any()<0.5 else 1-2*(1-grayImg)**2 
 
-print(newMatrix.shape)
+# print(newMatrix)
 
-newMatrix = np.floor(newMatrix*255)
+# newMatrix = np.floor(newMatrix*255)
 
-print(newMatrix)
+newMatrix2 = powerFunc(newMatrix)
 
-cv2.imwrite("D:\\house\\test.jpg",newMatrix)
+# print(newMatrix2)
+
+newMatrix3 = np.floor(minV + maxV*newMatrix2)
+
+print(newMatrix3)
+
+cv2.imwrite("D:\\homework\\homework\\house\\test.jpg",newMatrix3)
+
+newMatrix3 = np.array(newMatrix3,dtype=np.uint8)
 
 
-img2 = np.power(grayImg/255.0,2.2)
-newMatrix2 = np.floor(img2*255)
+# img2 = np.power(grayImg/255.0,2.2)
+# newMatrix2 = np.floor(img2*255)
 
-cv2.imwrite("D:\\house\\gamma.jpg",newMatrix)
+# cv2.imwrite("D:\\house\\gamma.jpg",newMatrix)
+
+
+grayImg1 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret1,th1 = cv2.threshold(grayImg1,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+# print(th1)
+
+cv2.imwrite("D:\\homework\\homework\\house\\b.jpg",th1)
 
