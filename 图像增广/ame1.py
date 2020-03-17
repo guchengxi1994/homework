a24fdb77302719d559c1765a61fb5897e2123249
guchengxi1994 <528490652@qq.com>
2020-03-17 16:55:08 +0800

    a

--- 图像增广/ame1.py
+++ 图像增广/ame1.py
@@ -1,12 +1,3 @@
-'''
-@lanhuage: python
-@Descripttion: 图像增广
-@version: beta
-@Author: xiaoshuyui
-@Date: 2020-03-11 10:09:17
-@LastEditors: xiaoshuyui
-@LastEditTime: 2020-03-17 16:50:38
-'''
 import cv2
 import math
 import numpy as np
@@ -101,7 +92,7 @@ class ImgAugemention():
         for f in flip_list:
             for img_name in os.listdir(imgs_path):
                 n, s = os.path.splitext(img_name)
-                if s == ".jpg" and n+".xml" in os.listdir(xmls_path):
+                if s == ".jpg":
                     img_path = os.path.join(imgs_path, img_name)
                     img = cv2.imread(img_path)
                     rotated_img = self.flip_img(img, f)
@@ -144,73 +135,6 @@ class ImgAugemention():
                         print("[%s] %s is processed." % (f, img_name))
 
 
-    
-    """
-    直方图均衡化
-    """
-
-    def his(self,src):
-        b, g, r = cv2.split(src)
-        # 创建局部直方图均衡化
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(5, 5))
-        # 对每一个通道进行局部直方图均衡化
-        b = clahe.apply(b)
-        g = clahe.apply(g)
-        r = clahe.apply(r)
-        # 合并处理后的三通道 成为处理后的图
-        image = cv2.merge([b, g, r])
-
-        return image
-
-    def _process_img_his(self, imgs_path, xmls_path, img_save_path, xml_save_path):
-
-        for img_name in os.listdir(imgs_path):
-                n, s = os.path.splitext(img_name)
-                if s == ".jpg" and n+".xml" in os.listdir(xmls_path):
-                    img_path = os.path.join(imgs_path, img_name)
-                    img = cv2.imread(img_path)
-                    # rotated_img = self.flip_img(img, f)
-                    his_img = self.his(img)
-                    # 写入图像
-                    cv2.imwrite(img_save_path + n + "_"  + "HIS.jpg", his_img)
-                    # print("log: [%sd] %s is processed." % (f, img))
-                    xml_url = img_name.split('.')[0] + '.xml'
-                    xml_path = os.path.join(xmls_path, xml_url)
-                    try:
-                        tree = ET.parse(xml_path)
-                        root = tree.getroot()
-
-                        root.find('folder').text = str(img_save_path.split(os.sep)[-1] if \
-                            img_save_path.split(os.sep)[-1] != "" else img_save_path.split(os.sep)[-2] )
-                        root.find('filename').text = str(n + "_" +  "HIS.jpg")
-                        root.find('path').text = str(img_save_path + n + "_" +  "HIS.jpg")
-
-
-
-                        for box in root.iter('bndbox'):
-                            xmin = float(box.find('xmin').text)
-                            ymin = float(box.find('ymin').text)
-                            xmax = float(box.find('xmax').text)
-                            ymax = float(box.find('ymax').text)
-                            # xmin,ymin,xmax,ymax = self.flip_xml(img, xmin, ymin, xmax, ymax, f)
-                            # change the coord
-                            box.find('xmin').text = str(int(xmin))
-                            box.find('ymin').text = str(int(ymin))
-                            box.find('xmax').text = str(int(xmax))
-                            box.find('ymax').text = str(int(ymax))
-                            box.set('updated', 'yes')
-                        # write into new xml
-                        tree.write(xml_save_path + n + "_" +  "HIS.xml")
-                        # print(xml_save_path + n + "_" + str(f) + "flip.xml")
-                    except Exception as e:
-                        # raise ValueError("no file")
-                        print(e)
-                        # pass
-                    finally:
-                        print("[%s] %s is processed." % ("HIS", img_name))
-
-
-
 
 
 
@@ -222,9 +146,8 @@ class ImgAugemention():
             for img_name in os.listdir(imgs_path):
                 # split filename and suffix
                 n, s = os.path.splitext(img_name)
-                print("================>"+n)
                 # for the sake of use yol model, only process '.jpg'
-                if s == ".jpg" and n+".xml" in os.listdir(xmls_path):
+                if s == ".jpg":
                     img_path = os.path.join(imgs_path, img_name)
                     img = cv2.imread(img_path)
                     rotated_img = self.rotate_image(img, angle)
@@ -269,13 +192,11 @@ class ImgAugemention():
 
 if __name__ == '__main__':
     img_aug = ImgAugemention()
-    imgs_path = 'D:\\getPianwei\\HBXZ\\'
-    xmls_path = 'D:\\getPianwei\\HBXZLabels\\'
-    # imgs_path = 'D:\\getPianwei\\test\\croppedIQI\\'
-    # xmls_path = 'D:\\getPianwei\\test\\IQIlabels\\'
+    imgs_path = 'D:\\getPianwei\\test\\croppedIQI\\'
+    xmls_path = 'D:\\getPianwei\\test\\IQIlabels\\'
     img_save_path = 'D:\\getPianwei\\AMEIMG\\'
     xml_save_path = 'D:\\getPianwei\\AMEXML\\'
     angle_list = [90, 180, 270]
     img_aug.process_img(imgs_path, xmls_path, img_save_path, xml_save_path, angle_list)
 
-    img_aug._process_img_flip(imgs_path, xmls_path, img_save_path, xml_save_path, [1,0,-1])
+    # img_aug._process_img_flip(imgs_path, xmls_path, img_save_path, xml_save_path, [1,0,-1])
