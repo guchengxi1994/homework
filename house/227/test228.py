de298c9f073a762fe6870d7945964c66ffa028eb
guchengxi1994 <528490652@qq.com>
2020-04-02 16:58:45 +0800

    a

--- house/227/test228.py
+++ house/227/test228.py
@@ -271,14 +271,37 @@ def process(imgPath):
 
     wL = np.sum(lcc,axis=1,dtype=np.float32)
     # wwl = (wL != 0)
-    wL[wL == 0] = np.NaN
-    w = np.nanmean(wL)
+    wL[wL == 0] = 1
+    # w = np.nanmean(wL)
+    print(wL.shape)
     # img[:,int(0.5*img.shape[1])] = 1
     # lcc = largestConnectComponent(img)
     # lcc = np.array(lcc,dtype=np.uint8)
 
-    return lcc,w,fea2
+    return lcc,1,fea2
     # return lcc,w,nomarlLize(fea2)
+
+
+def process2(im):
+    imgShape = im.shape 
+
+    # im = cv2.imread(imgPath)
+    im = his(im)
+    if imgShape[0]>imgShape[1]:
+        pass 
+    else:
+        trans_img = cv2.transpose(im)
+        im = cv2.flip(trans_img, 1)
+    im = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    imgIn = im / 255.0
+    imgIn = xdog(imgIn, binarize=True,k=20)
+    # im = local_threshold(im)
+
+    # cv2.imwrite("out33333.png", imgIn*im)
+    fe = imgIn * im 
+    fea2 = np.sum(fe,axis=1,dtype=np.float32)
+    # print(len(fea2))
+    return fea2
 
 
 def smooth(a,WSZ = 3):
@@ -322,12 +345,18 @@ if __name__ == '__main__':
     # p1 = 'D:\\testALg\\homework\\house\\227\\weld\\w1.jpg'
     # p2 = 'D:\\testALg\\homework\\house\\227\\weld\\w2.jpg'
 
-    p1 = 'D:\\getWeld\\results\\1068-B-4-16-0000.jpg'
-    p2 = 'D:\\getWeld\\results\\1108-B-30-26-0000.jpg'
+    # p1 = 'D:\\getWeld\\results\\uploadTime-15735481088070000.jpg'
+    # p2 = 'D:\\getWeld\\results\\uploadTime-15735477899070003.jpg'
+
+    p1 = 'D:\\getWeld\\results\\1146-B-53-16-0000.jpg'
+    p2 = 'D:\\getWeld\\results\\1068-B-4-16-0000.jpg'
 
 
     import cv2
     import matplotlib.pyplot as plt
+
+    i1 = imread(p1)
+    i2 = imread(p2)
 
     
 
@@ -337,40 +366,43 @@ if __name__ == '__main__':
     # im = imread('D:\\testALg\\homework\\house\\227\\1122.jpg')
 
     # i2 = copy.deepcopy(lcc)
-    i1,w1,fea1 = process(p1)
-    # print(w1)
-    i2,w2,fea2 = process(p2)
+    # i1,w1,fea1 = process(p1)
+    # # print(w1)
+    # i2,w2,fea2 = process(p2)
 
     
-    # lcc = lcc*255
+    # # lcc = lcc*255
 
-    # r1 = getShape(i1)
-    # r2 = getShape(i2)
+    # # r1 = getShape(i1)
+    # # r2 = getShape(i2)
 
-    r1 = nomarlLize(getShape(i1))
-    r2 = nomarlLize(getShape(i2))
+    # r1 = nomarlLize(getShape(i1))
+    # r2 = nomarlLize(getShape(i2))
 
-    # r1 = getShape(i1)
-    # r2 = getShape(i2)
-
-
-    # import fastDtw
+    # # r1 = getShape(i1)
+    # # r2 = getShape(i2)
 
 
-    r11 = r1[int(0.25*len(r1)):int(0.75*len(r1))]
-    r22 = r2[int(0.25*len(r2)):int(0.75*len(r2))]
+    # # import fastDtw
+
+
+    # r11 = r1[int(0.25*len(r1)):int(0.75*len(r1))]
+    # r22 = r2[int(0.25*len(r2)):int(0.75*len(r2))]
 
 
 
-    res = cv2.matchTemplate(r1, r22, cv2.TM_CCOEFF_NORMED)
-    min_val1, max_val, _, _ = cv2.minMaxLoc(res)
+    # res = cv2.matchTemplate(r1, r22, cv2.TM_CCOEFF_NORMED)
+    # min_val1, max_val, _, _ = cv2.minMaxLoc(res)
 
-    res2 = cv2.matchTemplate(r2, r11, cv2.TM_CCOEFF_NORMED)
-    min_val2, max_val2, _, _ = cv2.minMaxLoc(res2)
-    print(0.5*(max_val+max_val2))
+    # res2 = cv2.matchTemplate(r2, r11, cv2.TM_CCOEFF_NORMED)
+    # min_val2, max_val2, _, _ = cv2.minMaxLoc(res2)
+    # print(0.5*(max_val+max_val2))
+
+    fea1 =  process2(i1)
+    fea2 =  process2(i2)
     
-    r11 = fea1[int(0.25*len(r1)):int(0.75*len(r1))]
-    r22 = fea2[int(0.25*len(r2)):int(0.75*len(r2))]
+    r11 = fea1[int(0.25*len(fea1)):int(0.75*len(fea1))]
+    r22 = fea2[int(0.25*len(fea2)):int(0.75*len(fea2))]
 
     res = cv2.matchTemplate(fea1, r22, cv2.TM_CCOEFF_NORMED)
     min_val1, max_val, _, _ = cv2.minMaxLoc(res)
@@ -389,6 +421,9 @@ if __name__ == '__main__':
     # print(min_val)
 
     # print(d)
+
+    # imsave('out1.png', i1*255)
+    # imsave('out2.png', i2*255)
     
     y1 = fea1
     y2 = fea2
@@ -403,21 +438,7 @@ if __name__ == '__main__':
 
     plt.show()
 
-    # ssssss = cv2.compareHist(r1,r2,cv2.HISTCMP_BHATTACHARYYA)
-    # print(ssssss)
-
-    # from sdtw import SoftDTW
-    # from sdtw.distance import SquaredEuclidean
-
-    # D = SquaredEuclidean(r1.reshape(-1,1), r2.reshape(-1,1))
-    # sdtw = SoftDTW(D, gamma=1.0)
-    # value = sdtw.compute()
-    # E = sdtw.grad()
-    # G = D.jacobian_product(E)
-
-    # print(value)
-
-
+    
 
 
 
@@ -431,6 +452,5 @@ if __name__ == '__main__':
  
 
 
-    imsave('out1.png', i1*255)
-    imsave('out2.png', i2*255)
+
 
