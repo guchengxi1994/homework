357d3011b0c70595cb12d28d8edb78e74188eed5
guchengxi1994 <528490652@qq.com>
2020-04-27 15:15:56 +0800

    clean codes

--- house/227/testPyDCM.py
+++ house/227/testPyDCM.py
@@ -5,11 +5,16 @@
 @Author: xiaoshuyui
 @Date: 2020-04-13 17:25:49
 @LastEditors: xiaoshuyui
-@LastEditTime: 2020-04-13 17:27:27
+@LastEditTime: 2020-04-27 13:49:01
 '''
 import pydicom
 from pydicom import dcmread
+import numpy as np
 
 filename = "D:\\dump\\338\\pipelineCode-17ZZ040-weldingCode-QJSG-NJ01-B-17ZZ040018-M_0001.dcm"
 dataset = dcmread(filename)
-print(dataset.pixel_array)
+print(dataset.pixel_array.shape)
+
+print(np.max(dataset.pixel_array))
+print(np.min(dataset.pixel_array))
+
