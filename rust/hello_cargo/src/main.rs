f077b9af84182586bba24d3577de3833362ffa96
guchengxi1994 <528490652@qq.com>
2020-04-29 10:53:52 +0800

    rust

--- rust/hello_cargo/src/main.rs
+++ rust/hello_cargo/src/main.rs
@@ -1,6 +1,11 @@
 pub mod game;
+pub mod lib;
 
 fn main() {
     // println!("Hello, world!");
-    game::guess()
+    // game::guess()
+    lib::mod429::test::t2();
+    // let mut aa = lib::mod429::test::t2(18);
+    // println!("{}", aa);
+
 }
--- rust/hello_cargo/target/debug/deps/hello_cargo.d
+++ rust/hello_cargo/target/debug/deps/hello_cargo.d
@@ -1,6 +1,8 @@
-D:\testALg\homework\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs src\game.rs
+D:\testALg\homework\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs src\game.rs src\lib\mod.rs src\lib\mod429.rs
 
-D:\testALg\homework\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs src\game.rs
+D:\testALg\homework\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs src\game.rs src\lib\mod.rs src\lib\mod429.rs
 
 src\main.rs:
 src\game.rs:
+src\lib\mod.rs:
+src\lib\mod429.rs:
